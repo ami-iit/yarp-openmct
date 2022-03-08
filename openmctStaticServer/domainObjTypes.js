710af224c150f0a989b7a66bd01a79b3b6e312b1
Nuno Guedelha <nuno.guedelha@iit.it>
2022-03-08 10:41:56 +0000

    Define new Domain Object types and improve the types mapping with the existing telemetry entries
    - https://github.com/nasa/openmct/blob/master/API.md#object-attributes:
      "type: All objects in Open MCT have a type. Types allow you to form an ontology of knowledge and provide an abstraction for grouping, visualizing, and interpreting data."
    
    - https://github.com/nasa/openmct/blob/master/API.md#domain-object-types:
    
        The `addType` function accepts two arguments:
        * A `string` key identifying the type. This key is used when specifying a type
        for an object.  We recommend prefixing your types with a namespace to avoid
        conflicts with other plugins.
        * An object type specification. An object type definition supports the following
        attributes
            * `name`: a `string` naming this object type
            * `description`: a `string` specifying a longer-form description of this type
            * `initialize`: a `function` which initializes the model for new domain objects
            of this type. This can be used for setting default values on an object when
            it is instantiated.
            * `creatable`: A `boolean` indicating whether users should be allowed to create
            this type (default: `false`). This will determine whether the type appears
            in the `Create` menu.
            * `cssClass`: A `string` specifying a CSS class to apply to each representation
            of this object. This is used for specifying an icon to appear next to each
            object of this type.

--- openmctStaticServer/domainObjTypes.js
+++ openmctStaticServer/domainObjTypes.js
@@ -1,13 +1,31 @@
 const DOMAIN_OBJECTS_TYPES = {
     "yarpopenmct.sensormeas": {
         name: 'iCub Sensor Telemetry Entry',
-        description: 'Telemetry entry from one or multiple iCub sensors published on a single port.',
+        description: 'Sensor measurements from one or multiple iCub sensors published on a single port.',
+        icon: 'icon-telemetry',
+        reportSchedule: [ReportSchedule.Realtime,ReportSchedule.Historical]
+    },
+    "yarpopenmct.camimage": {
+        name: 'iCub Eye Camera Telemetry Entry',
+        description: 'Camera images from one of the iCub eye cameras published on a single port.',
+        icon: 'icon-telemetry',
+        reportSchedule: [ReportSchedule.Realtime,ReportSchedule.Historical]
+    },
+    "yarpopenmct.nwstatus": {
+        name: 'Network Satus Related Entry',
+        description: 'Telemetry entry related to the satus of the network hosting the robot and other connected interface systems.',
+        icon: 'icon-telemetry',
+        reportSchedule: [ReportSchedule.Realtime,ReportSchedule.Historical]
+    },
+    "yarpopenmct.wbdestimation": {
+        name: 'Whole-body Dynamics Estimation Entry',
+        description: 'Dynamic variable output from the whole-body dynamics estimations.',
         icon: 'icon-telemetry',
         reportSchedule: [ReportSchedule.Realtime,ReportSchedule.Historical]
     },
     "yarpopenmct.veccollectionmap": {
-        name: 'Walking Controller Telemetry Entry',
-        description: 'Telemetry entry from the Walking Controller related data published on a single port (e.g. internal variables), structured as a vector collection map.',
+        name: 'Walking Controller Internal Variable or Setpoint Entry',
+        description: 'Walking Controller internal variable or setpoint data published within a vector collection map on a single port.',
         icon: 'icon-telemetry',
         reportSchedule: [ReportSchedule.Realtime,ReportSchedule.Historical]
     }
