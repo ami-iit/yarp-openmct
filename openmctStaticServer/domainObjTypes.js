556832d79679cd21c8cfd165e75eff413bd1a626
Nuno Guedelha <nuno.guedelha@iit.it>
2022-03-08 10:53:00 +0000

    Updated the Domain Object Types icons
    The Domain Object Type field "icon" is a `cssClass`:
    > a string identifying a CSS class to apply to this clock when it's displayed in the UI. This will be used to represent the time system with an icon. There are a number of built-in icon classes [available in Open MCT](https://github.com/nasa/openmct/blob/master/src/styles/_glyphs.scss), ...

--- openmctStaticServer/domainObjTypes.js
+++ openmctStaticServer/domainObjTypes.js
@@ -8,25 +8,25 @@ const DOMAIN_OBJECTS_TYPES = {
     "yarpopenmct.camimage": {
         name: 'iCub Eye Camera Telemetry Entry',
         description: 'Camera images from one of the iCub eye cameras published on a single port.',
-        icon: 'icon-telemetry',
+        icon: 'icon-camera',
         reportSchedule: [ReportSchedule.Realtime,ReportSchedule.Historical]
     },
     "yarpopenmct.nwstatus": {
         name: 'Network Satus Related Entry',
         description: 'Telemetry entry related to the satus of the network hosting the robot and other connected interface systems.',
-        icon: 'icon-telemetry',
+        icon: 'icon-connectivity',
         reportSchedule: [ReportSchedule.Realtime,ReportSchedule.Historical]
     },
     "yarpopenmct.wbdestimation": {
         name: 'Whole-body Dynamics Estimation Entry',
         description: 'Dynamic variable output from the whole-body dynamics estimations.',
-        icon: 'icon-telemetry',
+        icon: 'icon-gear',
         reportSchedule: [ReportSchedule.Realtime,ReportSchedule.Historical]
     },
     "yarpopenmct.veccollectionmap": {
         name: 'Walking Controller Internal Variable or Setpoint Entry',
         description: 'Walking Controller internal variable or setpoint data published within a vector collection map on a single port.',
-        icon: 'icon-telemetry',
+        icon: 'icon-telemetry-aggregate',
         reportSchedule: [ReportSchedule.Realtime,ReportSchedule.Historical]
     }
 };
